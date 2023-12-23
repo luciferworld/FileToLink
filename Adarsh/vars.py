b3a0a0d2d0c7e24d0262a289bc683a3097c312a1
Ibrahim Khan <108752662+luciferworld@users.noreply.github.com>
2023-12-23 17:52:13 +0500

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -16,11 +16,11 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001805072460'))
-    PORT = int(getenv('PORT', 6566))
+    PORT = int(getenv('PORT', 443))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5390385209").split())  
-    NO_PORT = bool(getenv('NO_PORT', False))
+    NO_PORT = bool(getenv('NO_PORT', True))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'lucifer6985'))
     if 'DYNO' in environ:
@@ -29,7 +29,7 @@ class Var(object):
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', '20.244.6.103')) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
